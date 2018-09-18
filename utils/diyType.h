83f0bb8ee2e8e3b9e888ba8ba2fc6d09774406f9
qingniao <1284530619@qq.com>
2018-09-18 17:42:56 +0800

    子文件工具代码

---
+++ utils/diyType.h
@@ -0,0 +1,25 @@
+﻿//
+//  diyType.h
+//  testC
+//
+//  Created by 陈帅 on 2018/9/18.
+//
+
+#ifndef diyType_h
+#define diyType_h
+
+enum actionType {
+	atStart,
+	atMove,
+	atStop,
+	atOver,
+};
+
+enum direction {
+	up,
+	down,
+	left,
+	right,
+};
+
+#endif
---
+++ utils/file.h
@@ -0,0 +1,20 @@
+﻿//
+//  main.hpp
+//  testC
+//
+//  Created by 陈帅 on 2018/9/16.
+//
+
+#ifndef file_h
+#define file_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <map>
+
+using namespace std;
+
+map<int, void*> readCsvData(string fname, string pKey, string pValue = "");
+
+#endif /* file_hpp */
